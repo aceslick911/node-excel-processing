7740eeac86096ef05145d5882586948a43ca04b8
Angelo Perera <angelo.perera@nintex.com>
2020-05-30 11:57:41 +1000

    Tweaks to make module easier to use
    - Modernised functions to arrow functions
    - File uploaded notification
    - Web-browser friendly page at http://localhost:3000/
    - Upadted readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # node-excel-processing
+
 process excel file in node js using streams
+
+> Forked from https://github.com/riddheshganatra/node-excel-processing
+
+Changes:
+
+- Modernised functions to arrow functions
+- File uploaded notification
+- Web-browser friendly page at http://localhost:3000/
+
+# How to use
+
+1. run `yarn install`
+2. run `yarn start`
+3. visit http://localhost:3000/ and upload an excel file
+
+A download will immediately begin (throttled) that will add an additional column with a random integer to any excel file (.xlsx)
--- controllers/route.js
+++ controllers/route.js
@@ -1,9 +1,24 @@
-const Router = require('express').Router;
+const Router = require("express").Router;
 const router = Router();
 
-const controller =require('./main.controller');
+const controller = require("./main.controller");
 
 // router for processing excel
-router.post('/processExcel',controller.processExcel)
+router.post("/processExcel", controller.processExcel);
+router.get("/", (req, res) => {
+  res.send(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <TITLE>Excel Stream</TITLE>
+  </head>
+  <body>
+  <form action="/processExcel" method="POST" enctype="multipart/form-data">
+  <input type="file" value="file" name="file" />
+  <input type="submit" value="Submit"/>
+  </form>
+  </body>
+  </html>
+  `);
+});
 
 module.exports = router;
